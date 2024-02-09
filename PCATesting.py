b043c7877b213d6dc075d3e5cdfa37df3c77a95e
John Stock <jmks1000@gmail.com>
2024-02-09 16:30:09 -0600

    latest experiment

--- PCATesting.py
+++ PCATesting.py
@@ -8,9 +8,17 @@ tool = TIFDataTool.Tools('master_TIF_UPDATED.csv')
 df = tool.getDF()
 df = df.drop(['tif_name','bank'], axis=1)
 
-features = ['property_tax_extraction', 'cumulative_property_tax_extraction', 'transfers_in', 'cumulative_transfers_in', 'expenses', 'fund_balance_end', 'transfers_out', 'distribution', 'admin_costs']
+features = ['property_tax_extraction', 
+            'cumulative_property_tax_extraction', 
+            'transfers_in', 
+            'cumulative_transfers_in', 
+            'expenses', 
+            'fund_balance_end', 
+            'transfers_out', 
+            'distribution', 
+            'finance_costs']
 xs = df.loc[:, features]
-ys = df.loc[:,['finance_costs']].values
+ys = df.loc[:,['admin_costs']].values
 df = StandardScaler().fit_transform(xs)
 
 pca = PCA(n_components=3)
