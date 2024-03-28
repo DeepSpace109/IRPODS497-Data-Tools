c9eca9292b42a56a42b432f652a30f056f729ee9
John Stock <jmks1000@gmail.com>
2024-03-28 11:02:55 -0500

    initial testing

--- awstesting.py
+++ awstesting.py
@@ -1,32 +1,30 @@
 import pymysql
-import sys
-import boto3
-import os
-import mysql.connector
-from mysql.connector import Error
 import csv
-import json
 
 #import secure read-only credentials to the database.
+#MUST HAVE awscreds.csv EXISTING WITH VALID CREDENTIALS
 with open('awscreds.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     
     for row in spamreader:
         creds = (row)
 
+#establish connection to DB and create cursor.
 try:
     conn = pymysql.connect(
         host=creds[0],
         user=creds[1],
         password=creds[2],
         database="chicago_TIF"
-    )   
+    )
+    cursor = conn.cursor()
 except pymysql.Error as e:
     print(e)
 
 #Chicago_TIF capital C
-sql = "describe Chicago_TIF;"
-cursor = conn.cursor()
+sql = "SHOW COLUMNS FROM Chicago;"
+"SELECT * FROM Chicago_TIF WHERE tif_name = '35th/Halsted';"
+
 cursor.execute(sql)
 results = cursor.fetchall()
 print(results)
